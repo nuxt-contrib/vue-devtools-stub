e07974195ff2b8f2a198b2878f47225241940efe
Pooya Parsa <pooya@pi0.io>
2022-08-17 14:56:15 +0200

    fix: add missing `default` and `__esModule` exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ export const setupDevtoolsPlugin = () => {}
 export const isPerformanceSupported = () => false
 
 export const now = () => Date.now()
+
+export const __esModule = true
+
+export default {
+  setupDevtoolsPlugin,
+  isPerformanceSupported,
+  now
+}
