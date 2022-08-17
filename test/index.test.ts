0a1ba2e8fb7caf7dc292fce2722d4535688798e2
Pooya Parsa <pooya@pi0.io>
2022-08-17 14:56:34 +0200

    test: add test to compare exports

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,15 @@
 import { expect, it, describe } from 'vitest'
+import * as actualDevtools from '@vue/devtools-api'
 import * as devtoolsAPI from '../src'
 
 describe('vue-devtools-stub', () => {
+  describe('has same exports of @vue/devtools-api', () => {
+    for (const key in actualDevtools) {
+      it(key, () => {
+        expect(typeof (devtoolsAPI as any)[key]).toBe(typeof (actualDevtools as any)[key])
+      })
+    }
+  })
   it('setupDevtoolsPlugin', () => {
     expect(devtoolsAPI.setupDevtoolsPlugin()).toBe(undefined)
   })
